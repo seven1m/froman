ea9005a74d625b5779ce29a3a678d6d61fe5f895
Tim Morgan <tim@timmorgan.org>
2017-04-18 23:33:29 -0500

    Refactor

--- src/runner.rs
+++ src/runner.rs
@@ -1,5 +1,5 @@
 extern crate redis;
-extern crate cmdline_words_parser;
+extern crate chrono;
 extern crate nix;
 
 use workers::*;
@@ -8,39 +8,60 @@ use std::thread::sleep;
 use std::time::Duration;
 use std::collections::HashMap;
 use std::process::{Command, Stdio, Child};
-use self::cmdline_words_parser::StrExt;
-use self::nix::sys::signal::kill;
-use self::nix::sys::signal::Signal;
+use std::io::Write;
+use self::chrono::prelude::*;
+use self::nix::sys::signal::{kill, Signal};
+
+struct RunningProcess {
+    process: Child,
+    terminate_at: Option<DateTime<Local>>
+}
 
 pub fn run(workers: &Vec<Box<Worker>>, config_dir: &str, command_template: &str, redis_url: &str) {
     let interval = Duration::from_secs(2);
     let redis = redis::Client::open(redis_url).unwrap();
     let redis_conn = redis.get_connection().unwrap();
-    let mut processes: HashMap<String, Child> = HashMap::new();
+    let mut processes: HashMap<String, RunningProcess> = HashMap::new();
     loop {
         for worker in workers {
-            let key = format!("{}: {}", worker.app(), worker.kind()).to_string();
+            let key = worker.key();
             if worker.work_to_do(&redis_conn) || worker.work_being_done(&redis_conn) {
-                if !processes.contains_key(&key) {
-                    let command = command_template.replace("%s", &format!("cd {} && {}", worker.path(), &worker.command()));
-                    let mut command_to_parse = command.clone();
-                    let mut args: Vec<&str> = command_to_parse.parse_cmdline_words().collect();
-                    let program = args.remove(0);
-                    println!("spawn program {} with args {:?} at path {}", &program, &args, &config_dir);
-                    let child = Command::new(&program)
-                        .args(args)
-                        //.stdin(Stdio::piped())
-                        //.stdout(Stdio::piped())
-                        .current_dir(config_dir)
-                        .spawn()
-                        .expect(&format!("Failed to execute command {}", &command));
-                    processes.insert(key, child);
-                }
-            } else {
                 if processes.contains_key(&key) {
-                    println!("removing process {}", &key);
-                    let process = processes.remove(&key).unwrap();
-                    kill(process.id() as i32, Signal::SIGTERM).unwrap();
+                    let mut running_process = processes.get_mut(&key).unwrap();
+                    println!("more work found; setting terminate_at to None");
+                    running_process.terminate_at = None
+                } else {
+                    let process = worker.spawn(command_template, config_dir);
+                    let running_process = RunningProcess {
+                        process: process,
+                        terminate_at: None
+                    };
+                    processes.insert(key, running_process);
+                }
+            } else if processes.contains_key(&key) {
+                let now = Local::now();
+                let mut remove = false;
+                {
+                    let mut running_process = processes.get_mut(&key).unwrap();
+                    match running_process.terminate_at {
+                        Some(terminate_at) => {
+                            if terminate_at <= now {
+                                println!("terminate_at is before {:?}", now);
+                                kill(running_process.process.id() as i32, Signal::SIGTERM).unwrap();
+                                remove = true;
+                            } else {
+                                println!("terminate_at is after {:?}", now);
+                            }
+                        }
+                        None => {
+                            let terminate_at = now + chrono::Duration::seconds(10);
+                            println!("setting terminate_at to {:?}", terminate_at);
+                            running_process.terminate_at = Some(terminate_at);
+                        }
+                    }
+                }
+                if remove {
+                    processes.remove(&key);
                 }
             }
         }
--- src/workers.rs
+++ src/workers.rs
@@ -1,14 +1,40 @@
 extern crate redis;
+extern crate cmdline_words_parser;
+
 use self::redis::Commands;
+use self::cmdline_words_parser::StrExt;
+use std::process::{Command, Stdio, Child};
 
 pub trait Worker {
     fn app(&self) -> &String;
     fn path(&self) -> &String;
-    fn namespace(&self) -> &String;
     fn command(&self) -> &String;
-    fn kind(&self) -> String;
+    fn kind(&self) -> &str;
     fn work_to_do(&self, &redis::Connection) -> bool;
     fn work_being_done(&self, &redis::Connection) -> bool;
+
+    fn command_binary_and_args(&self, command_template: &str) -> (String, Vec<String>) {
+        let mut command_to_parse = command_template.replace("%s", &format!("cd {} && {}", self.path(), self.command()));
+        let mut args: Vec<String> = command_to_parse.parse_cmdline_words().map(|a| a.to_string()).collect();
+        let program = args.remove(0).to_string();
+        (program, args)
+    }
+
+    fn key(&self) -> String {
+        format!("{}: {}", self.app(), self.kind())
+    }
+
+    fn spawn(&self, command_template: &str, config_dir: &str) -> Child {
+        let (program, args) = self.command_binary_and_args(command_template);
+        println!("spawn process {} with args {:?} at path {}", &program, &args, &config_dir);
+        Command::new(&program)
+            .args(args)
+            .stdin(Stdio::piped())
+            //.stdout(Stdio::piped())
+            .current_dir(config_dir)
+            .spawn()
+            .expect(&format!("Failed to execute command {}", self.command()))
+    }
 }
 
 pub struct Sidekiq {
@@ -27,16 +53,12 @@ impl Worker for Sidekiq {
         &self.path
     }
 
-    fn namespace(&self) -> &String {
-        &self.namespace
-    }
-
     fn command(&self) -> &String {
         &self.command
     }
 
-    fn kind(&self) -> String {
-        "sidekiq".to_string()
+    fn kind(&self) -> &str {
+        "sidekiq"
     }
 
     fn work_to_do(&self, redis_conn: &redis::Connection) -> bool {
@@ -72,16 +94,12 @@ impl Worker for Resque {
         &self.path
     }
 
-    fn namespace(&self) -> &String {
-        &self.namespace
-    }
-
     fn command(&self) -> &String {
         &self.command
     }
 
-    fn kind(&self) -> String {
-        "resque".to_string()
+    fn kind(&self) -> &str {
+        "resque"
     }
 
     fn work_to_do(&self, redis_conn: &redis::Connection) -> bool {
